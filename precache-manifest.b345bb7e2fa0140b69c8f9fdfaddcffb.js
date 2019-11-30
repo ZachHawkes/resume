76a51ef04991ee13eb6794bd036b963a2e3f5cb7
Zach Hawkes <zach@ifit.com>
2019-11-30 12:20:27 -0700

    Updates

---
+++ precache-manifest.b345bb7e2fa0140b69c8f9fdfaddcffb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0b4b320f275425b4906c17f6e7841fd",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "ba04370599d47665b241",
+    "url": "/resume/static/css/main.82edd4a9.chunk.css"
+  },
+  {
+    "revision": "c392004c916de823d000",
+    "url": "/resume/static/js/2.15daf873.chunk.js"
+  },
+  {
+    "revision": "ba04370599d47665b241",
+    "url": "/resume/static/js/main.66d3dfaf.chunk.js"
+  },
+  {
+    "revision": "431263ebd9671701aa27",
+    "url": "/resume/static/js/runtime-main.c535cb89.js"
+  }
+]);
